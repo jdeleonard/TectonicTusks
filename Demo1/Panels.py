4558eeaf53396163bcd895ea145de4046658aebf
Jacob Rydecki <98055466+jrydecki@users.noreply.github.com>
2022-03-24 23:09:11 -0600

    Created Demo1 Directory

---
+++ Demo1/Panels.py
@@ -0,0 +1,53 @@
+import wx, sqlite3
+
+from InventoryGrid import *
+
+class InventoryPanel(wx.Panel):
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent=parent)
+        myGrid = MyGrid(self)
+        sizer = wx.BoxSizer(wx.HORIZONTAL)
+        sizer.Add(myGrid, 1)
+        self.SetSizer(sizer)
+
+
+class InsertionPanel(wx.Panel):
+
+    def insertItem(self, event):
+
+        conn = opendb("food.db")
+
+        id = self.idText.GetValue()
+        food = self.foodText.GetValue()
+        unit = self.unitText.GetValue()
+        edate = self.edateText.GetValue()
+
+        if (id != ""):
+            insertRow(conn, id, food, unit, edate)
+
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent=parent)
+
+        idLabel = wx.StaticText(self, -1, "ID: ")
+        self.idText = wx.TextCtrl(self, -1)
+
+        foodLabel = wx.StaticText(self, -1, "Food Name: ")
+        self.foodText = wx.TextCtrl(self, -1)
+
+        unitLabel = wx.StaticText(self, -1, "Unit: ")
+        self.unitText = wx.TextCtrl(self, -1)
+
+        edateLabel = wx.StaticText(self, -1, "Expiration Date: ")
+        self.edateText = wx.TextCtrl(self, -1)
+
+        button = wx.Button(self, label="Add Item", size=(75,75))
+        self.Bind(wx.EVT_BUTTON, self.insertItem, button)
+
+        sizer = wx.FlexGridSizer(cols=2, hgap=6, vgap=6)
+        sizer.AddMany([idLabel,self.idText,foodLabel,self.foodText,unitLabel,self.unitText,edateLabel,self.edateText, button])
+    
+
+        
+
+
+        self.SetSizer(sizer)
